7821d4466cf9580cdee6e7ce2c2cf30f50f5e91a
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2021-01-09 14:05:22 -0600

    (#25) Organizes classes and helper functions

--- gophercannon/gophercannon.py
+++ gophercannon/gophercannon.py
@@ -5,20 +5,13 @@ from jinja2 import Template
 import argparse
 import time
 import tabulate
-from random import random
 import matplotlib.pyplot as plt
 import os
 from json2html import *
 from functools import partial
 import ntpath
-
-MICROSECONDS_PER_SECOND = 1000000
-
-RESPONSE_PREAMBLE = 1 + 11 + 29 + 1 + 1 + 1 + 1 + 2 + 4
-RESPONSE_EPILOGUE = 15 + 1 + 1 + 1 + 7 + 3
-
-REQUEST_PREAMBLE = 1 + 11 + 29 + 1 + 1 + 1 + 1 + 2
-REQUEST_EPILOGUE = 15 + 1 + 1 + 1 + 7 + 3
+import gophercannon_lib
+from gophercannon_lib import Module, Bus, Message
 
 parser = argparse.ArgumentParser(prog='gophercannon.py',
     description='Program to generate GopherCAN configuration files and perform static network validation.')
@@ -81,103 +74,9 @@ args = parser.parse_args()
 filename = args.network_file
 
 HASH_LENGTH = 16 # Use as many hexits as will fit in a single message
-MICROSECONDS_PER_SECOND = 1000000
 
 repo = git.Repo(search_parent_directories=True)
 repo_hash = repo.head.object.hexsha[0:HASH_LENGTH]
-
-def get_gophercan_id(priority, destination_node, source_node, id):
-    return (priority << 28) | (destination_node << 22) | (source_node << 16) | (id)
-
-def my_html_row_with_attrs(celltag, cell_values, colwidths, colaligns):
-    alignment = { "left":    '',
-                  "right":   ' style="text-align: right;"',
-                  "center":  ' style="text-align: center;"',
-                  "decimal": ' style="text-align: right;"' }
-    values_with_attrs =\
-        ["<{0}{1} class=\"my-cell\">{2}</{0}>"
-            .format(celltag, alignment.get(a, ''), c)
-         for c, a in zip(cell_values, colaligns)]
-    return "<tr class=\"my-row\">" + \
-            "".join(values_with_attrs).rstrip() + \
-            "</tr>"
-
-class Module:
-    def __init__(self, name, fifo_mailbox, noise):
-        self.name = name
-        self.fifo_mailbox = fifo_mailbox
-        self.noise = noise
-        self.messages = []
-        self.tx_buffer = []
-    
-    def add_message(self, message):
-        self.messages.append(message)
-    
-    def tick(self):
-        for message in self.messages:
-            message.timer -= 1
-
-        for message in self.messages:
-            if message.timer <= 0:
-                self.tx_buffer.append(message)
-                self.tx_buffer.sort(key=lambda x: get_gophercan_id( int(x.priority), 
-                                                                    int(x.producer),
-                                                                    int(x.consumer),
-                                                                    int(x.id)), reverse=False)
-                message.timer = message.reset_value + (random() - 0.5) * args.wobble # Simulate sway in message timing
-        
-        for message in self.tx_buffer:
-            message.buffer_delay += 1
-
-class Message:
-    def __init__(self, name, length, priority, frequency, producer, consumer, id):
-        self.name = name
-        self.length = length
-        self.priority = priority
-        self.frequency = frequency
-        self.producer = producer
-        self.consumer = consumer
-        self.id = id
-        self.timer = 0
-        self.buffer_delay = 0
-        self.reset_value = MICROSECONDS_PER_SECOND / frequency
-        self.buffer_delays = []
-    
-    def get_length(self):
-        return REQUEST_PREAMBLE + REQUEST_EPILOGUE + RESPONSE_PREAMBLE + (8 * self.length) + RESPONSE_EPILOGUE
-
-class Bus:
-    def __init__(self, name, baud_rate):
-        self.name = name
-        self.baud_rate = baud_rate
-        self.modules = []
-        self.timer = 0
-        self.master_timer = 0
-        self.idle_timer = 0
-        self.line = []
-        self.arbitrators = []
-    
-    def add_module(self, module):
-        self.modules.append(module)
-
-    def simulate(self):
-        competing_modules = [x for x in self.modules if len(x.tx_buffer) > 0]
-        self.arbitrators.append(len(competing_modules))
-        if self.timer > 0:
-            self.timer -= 1
-        else:
-            if len(competing_modules) > 0:
-                competing_modules.sort(key=lambda x: get_gophercan_id(  int(x.tx_buffer[0].priority), 
-                                                                        int(x.tx_buffer[0].producer),
-                                                                        int(x.tx_buffer[0].consumer),
-                                                                        int(x.tx_buffer[0].id)), reverse=False)
-                mod = competing_modules.pop(0)
-                msg = mod.tx_buffer.pop(0)
-                msg.buffer_delays.append(msg.buffer_delay)
-                msg.buffer_delay = 0
-                self.timer = msg.get_length() - 1 # We spent one time quanta arbitrating
-            else:
-                self.idle_timer += 1
 
 print("Welcome to GopherCANnon.")
 
@@ -273,7 +172,11 @@ with open(filename) as can_file:
             print("\nAnalyzing steady-state traffic on {0} from consumed parameters.".format(bus.name))
             bits_per_second = 0
             for parameter in consumed:
-                bits_per_request = REQUEST_PREAMBLE + REQUEST_EPILOGUE + RESPONSE_PREAMBLE + RESPONSE_EPILOGUE + 8 * parameters[parameter]["bytes"]
+                bits_per_request = (gophercannon_lib.REQUEST_PREAMBLE + 
+                                    gophercannon_lib.REQUEST_EPILOGUE + 
+                                    gophercannon_lib.RESPONSE_PREAMBLE + 
+                                    gophercannon_lib.RESPONSE_EPILOGUE + 
+                                    8 * parameters[parameter]["bytes"])
                 bits_per_second += bits_per_request * parameters[parameter]["frequency"]
             
             steady_usage = 100 * bits_per_second / bus.baud_rate
@@ -289,7 +192,7 @@ with open(filename) as can_file:
             i = 0
             for module in buss.modules:
                 if module['parameters_consumed']:
-                    sim_modules.append(Module(module, True, 10))
+                    sim_modules.append(Module(module, True, args.wobble))
                     for parameter in module['parameters_consumed']:
                         sim_modules[i].add_message(Message( parameter, 
                                                             parameters[parameter]['bytes'], 
@@ -308,7 +211,7 @@ with open(filename) as can_file:
                     module.tick()
                 bus.simulate()
                 bus.master_timer += 1
-                if bus.master_timer >= MICROSECONDS_PER_SECOND:
+                if bus.master_timer >= gophercannon_lib.MICROSECONDS_PER_SECOND:
                     i += 1
                     print("Simulated {0} seconds of traffic.".format(i))
                     bus.idle_timer = 0
@@ -319,9 +222,9 @@ with open(filename) as can_file:
                     module.tick()
                 bus.simulate()
                 bus.master_timer += 1
-                if bus.master_timer >= MICROSECONDS_PER_SECOND:
+                if bus.master_timer >= gophercannon_lib.MICROSECONDS_PER_SECOND:
                     i += 1
-                    transient_usage = 100 * (MICROSECONDS_PER_SECOND - bus.idle_timer) / bus.master_timer
+                    transient_usage = 100 * (gophercannon_lib.MICROSECONDS_PER_SECOND - bus.idle_timer) / bus.master_timer
                     print("Utilization over the last second: {0}%".format(transient_usage))
                     bus.idle_timer = 0
                     bus.master_timer = 0
@@ -387,8 +290,8 @@ with open(filename) as can_file:
             linebelowheader=None,
             linebetweenrows=None,
             linebelow=tabulate.Line("</table>", "", "", ""),
-            headerrow=partial(my_html_row_with_attrs, "th"),
-            datarow=partial(my_html_row_with_attrs, "td"),
+            headerrow=partial(gophercannon_lib.my_html_row_with_attrs, "th"),
+            datarow=partial(gophercannon_lib.my_html_row_with_attrs, "td"),
             padding=0, with_header_hide=None)
         transient_table = tabulate.tabulate(message_table, [ "Parameter", 
                                                     "TX Count", 
