581283ee0e2974e54688376e5565f69a1debd956
Calvin Molitor <52789184+caljay98@users.noreply.github.com>
2022-09-18 18:24:29 -0500

    attempting to fix the merge conflict again

---
+++ GopherCAN_ids.c
@@ -0,0 +1,627 @@
+// start of GopherCAN_ids.c
+//  (auto-generated)
+//  These files are used in GopherCAN.c using an extern in GopherCAN_ids.h
+
+#include "GopherCAN.h"
+
+// all of the global parameter structs
+CAN_COMMAND_STRUCT can_command = {0};
+FLOAT_CAN_STRUCT bat_0_temp = {0};
+FLOAT_CAN_STRUCT bat_1_temp = {0};
+FLOAT_CAN_STRUCT bat_2_temp = {0};
+FLOAT_CAN_STRUCT bat_3_temp = {0};
+FLOAT_CAN_STRUCT bat_4_temp = {0};
+FLOAT_CAN_STRUCT bat_5_temp = {0};
+FLOAT_CAN_STRUCT bat_6_temp = {0};
+FLOAT_CAN_STRUCT bat_7_temp = {0};
+FLOAT_CAN_STRUCT bat_8_temp = {0};
+FLOAT_CAN_STRUCT bat_9_temp = {0};
+FLOAT_CAN_STRUCT bat_10_temp = {0};
+FLOAT_CAN_STRUCT bat_11_temp = {0};
+FLOAT_CAN_STRUCT bat_12_temp = {0};
+FLOAT_CAN_STRUCT bat_13_temp = {0};
+FLOAT_CAN_STRUCT bat_14_temp = {0};
+FLOAT_CAN_STRUCT bat_15_temp = {0};
+FLOAT_CAN_STRUCT bat_16_temp = {0};
+FLOAT_CAN_STRUCT bat_17_temp = {0};
+FLOAT_CAN_STRUCT bat_18_temp = {0};
+FLOAT_CAN_STRUCT bat_19_temp = {0};
+FLOAT_CAN_STRUCT bat_20_temp = {0};
+FLOAT_CAN_STRUCT bat_21_temp = {0};
+FLOAT_CAN_STRUCT bat_22_temp = {0};
+FLOAT_CAN_STRUCT bat_23_temp = {0};
+FLOAT_CAN_STRUCT bat_24_temp = {0};
+FLOAT_CAN_STRUCT bat_25_temp = {0};
+FLOAT_CAN_STRUCT bat_26_temp = {0};
+FLOAT_CAN_STRUCT bat_27_temp = {0};
+FLOAT_CAN_STRUCT bat_28_temp = {0};
+FLOAT_CAN_STRUCT bat_29_temp = {0};
+FLOAT_CAN_STRUCT bat_30_temp = {0};
+FLOAT_CAN_STRUCT bat_31_temp = {0};
+FLOAT_CAN_STRUCT bat_32_temp = {0};
+FLOAT_CAN_STRUCT bat_33_temp = {0};
+FLOAT_CAN_STRUCT bat_34_temp = {0};
+FLOAT_CAN_STRUCT bat_35_temp = {0};
+FLOAT_CAN_STRUCT bat_36_temp = {0};
+FLOAT_CAN_STRUCT bat_37_temp = {0};
+FLOAT_CAN_STRUCT bat_38_temp = {0};
+FLOAT_CAN_STRUCT bat_39_temp = {0};
+FLOAT_CAN_STRUCT bat_40_temp = {0};
+FLOAT_CAN_STRUCT bat_41_temp = {0};
+FLOAT_CAN_STRUCT bat_42_temp = {0};
+FLOAT_CAN_STRUCT bat_43_temp = {0};
+FLOAT_CAN_STRUCT bat_44_temp = {0};
+FLOAT_CAN_STRUCT bat_45_temp = {0};
+FLOAT_CAN_STRUCT bat_46_temp = {0};
+FLOAT_CAN_STRUCT bat_47_temp = {0};
+FLOAT_CAN_STRUCT bat_48_temp = {0};
+FLOAT_CAN_STRUCT bat_49_temp = {0};
+FLOAT_CAN_STRUCT bat_50_temp = {0};
+FLOAT_CAN_STRUCT bat_51_temp = {0};
+FLOAT_CAN_STRUCT bat_52_temp = {0};
+FLOAT_CAN_STRUCT bat_53_temp = {0};
+FLOAT_CAN_STRUCT bat_54_temp = {0};
+FLOAT_CAN_STRUCT bat_55_temp = {0};
+FLOAT_CAN_STRUCT bat_56_temp = {0};
+FLOAT_CAN_STRUCT bat_57_temp = {0};
+FLOAT_CAN_STRUCT bat_58_temp = {0};
+FLOAT_CAN_STRUCT bat_59_temp = {0};
+FLOAT_CAN_STRUCT bat_60_temp = {0};
+FLOAT_CAN_STRUCT bat_61_temp = {0};
+FLOAT_CAN_STRUCT bat_62_temp = {0};
+FLOAT_CAN_STRUCT bat_63_temp = {0};
+FLOAT_CAN_STRUCT bat_64_temp = {0};
+FLOAT_CAN_STRUCT bat_65_temp = {0};
+FLOAT_CAN_STRUCT bat_66_temp = {0};
+FLOAT_CAN_STRUCT bat_67_temp = {0};
+FLOAT_CAN_STRUCT bat_68_temp = {0};
+FLOAT_CAN_STRUCT bat_69_temp = {0};
+FLOAT_CAN_STRUCT bat_70_temp = {0};
+FLOAT_CAN_STRUCT bat_71_temp = {0};
+FLOAT_CAN_STRUCT bat_72_temp = {0};
+FLOAT_CAN_STRUCT bat_73_temp = {0};
+FLOAT_CAN_STRUCT bat_74_temp = {0};
+FLOAT_CAN_STRUCT bat_75_temp = {0};
+FLOAT_CAN_STRUCT bat_76_temp = {0};
+FLOAT_CAN_STRUCT bat_77_temp = {0};
+FLOAT_CAN_STRUCT bat_78_temp = {0};
+FLOAT_CAN_STRUCT bat_79_temp = {0};
+FLOAT_CAN_STRUCT bat_80_temp = {0};
+FLOAT_CAN_STRUCT bat_81_temp = {0};
+FLOAT_CAN_STRUCT bat_82_temp = {0};
+FLOAT_CAN_STRUCT bat_83_temp = {0};
+FLOAT_CAN_STRUCT bat_84_temp = {0};
+FLOAT_CAN_STRUCT bat_85_temp = {0};
+FLOAT_CAN_STRUCT bat_86_temp = {0};
+FLOAT_CAN_STRUCT bat_87_temp = {0};
+FLOAT_CAN_STRUCT bat_min_temp = {0};
+FLOAT_CAN_STRUCT bat_ave_temp = {0};
+FLOAT_CAN_STRUCT bat_max_temp = {0};
+FLOAT_CAN_STRUCT pdm_1_current = {0};
+FLOAT_CAN_STRUCT pdm_2_current = {0};
+FLOAT_CAN_STRUCT pdm_3_current = {0};
+FLOAT_CAN_STRUCT pdm_4_current = {0};
+FLOAT_CAN_STRUCT pdm_5_current = {0};
+FLOAT_CAN_STRUCT pdm_6_current = {0};
+FLOAT_CAN_STRUCT pdm_7_current = {0};
+FLOAT_CAN_STRUCT pdm_8_current = {0};
+FLOAT_CAN_STRUCT pdm_9_current = {0};
+FLOAT_CAN_STRUCT pdm_10_current = {0};
+FLOAT_CAN_STRUCT pdm_11_current = {0};
+FLOAT_CAN_STRUCT pdm_12_current = {0};
+FLOAT_CAN_STRUCT pdm_13_current = {0};
+FLOAT_CAN_STRUCT pdm_14_current = {0};
+FLOAT_CAN_STRUCT pdm_15_current = {0};
+FLOAT_CAN_STRUCT pdm_16_current = {0};
+FLOAT_CAN_STRUCT pdm_17_current = {0};
+FLOAT_CAN_STRUCT pdm_18_current = {0};
+FLOAT_CAN_STRUCT pdm_19_current = {0};
+FLOAT_CAN_STRUCT pdm_20_current = {0};
+FLOAT_CAN_STRUCT pdm_voltage = {0};
+FLOAT_CAN_STRUCT pdm_temp = {0};
+FLOAT_CAN_STRUCT vcu_apps1 = {0};
+FLOAT_CAN_STRUCT vcu_apps2 = {0};
+FLOAT_CAN_STRUCT vcu_current1 = {0};
+FLOAT_CAN_STRUCT vcu_current2 = {0};
+FLOAT_CAN_STRUCT vcu_brake_pressure_front = {0};
+FLOAT_CAN_STRUCT vcu_wsfl = {0};
+FLOAT_CAN_STRUCT vcu_wsfr = {0};
+FLOAT_CAN_STRUCT vcu_spfl = {0};
+FLOAT_CAN_STRUCT vcu_spfr = {0};
+FLOAT_CAN_STRUCT vcu_sprl = {0};
+FLOAT_CAN_STRUCT vcu_sprr = {0};
+FLOAT_CAN_STRUCT vcu_sas = {0};
+FLOAT_CAN_STRUCT vcu_bat_volts = {0};
+U8_CAN_STRUCT vcu_rtd_btn = {0};
+FLOAT_CAN_STRUCT vcu_rtd_bzzr_duty = {0};
+FLOAT_CAN_STRUCT vcu_pump_duty = {0};
+FLOAT_CAN_STRUCT vcu_fan_duty = {0};
+FLOAT_CAN_STRUCT vcu_bl_duty = {0};
+U32_CAN_STRUCT vcu_status = {0};
+FLOAT_CAN_STRUCT inv_temp_igbt_a = {0};
+FLOAT_CAN_STRUCT inv_temp_igbt_b = {0};
+FLOAT_CAN_STRUCT inv_temp_igbt_c = {0};
+FLOAT_CAN_STRUCT inv_temp_gate_driver = {0};
+FLOAT_CAN_STRUCT inv_temp_ctrl_board = {0};
+FLOAT_CAN_STRUCT inv_temp_rtd_1 = {0};
+FLOAT_CAN_STRUCT inv_temp_rtd_2 = {0};
+FLOAT_CAN_STRUCT inv_temp_rtd_3 = {0};
+FLOAT_CAN_STRUCT inv_temp_rtd_4 = {0};
+FLOAT_CAN_STRUCT inv_temp_rtd_5 = {0};
+FLOAT_CAN_STRUCT inv_motor_temp = {0};
+FLOAT_CAN_STRUCT inv_torque_shudder = {0};
+U8_CAN_STRUCT inv_digital_in_1 = {0};
+U8_CAN_STRUCT inv_digital_in_2 = {0};
+U8_CAN_STRUCT inv_digital_in_3 = {0};
+U8_CAN_STRUCT inv_digital_in_4 = {0};
+U8_CAN_STRUCT inv_digital_in_5 = {0};
+U8_CAN_STRUCT inv_digital_in_6 = {0};
+U8_CAN_STRUCT inv_digital_in_7 = {0};
+U8_CAN_STRUCT inv_digital_in_8 = {0};
+FLOAT_CAN_STRUCT inv_motor_angle = {0};
+U16_CAN_STRUCT inv_motor_speed = {0};
+FLOAT_CAN_STRUCT inv_motor_ac_freq = {0};
+FLOAT_CAN_STRUCT inv_delta_resolver_filtered = {0};
+FLOAT_CAN_STRUCT inv_current_phase_a = {0};
+FLOAT_CAN_STRUCT inv_current_phase_b = {0};
+FLOAT_CAN_STRUCT inv_current_phase_c = {0};
+FLOAT_CAN_STRUCT inv_current_dc_bus = {0};
+FLOAT_CAN_STRUCT inv_dc_bus_voltage = {0};
+FLOAT_CAN_STRUCT inv_peak_output_voltage = {0};
+FLOAT_CAN_STRUCT inv_vd_voltage = {0};
+FLOAT_CAN_STRUCT inv_vq_voltage = {0};
+FLOAT_CAN_STRUCT inv_flux_cmd = {0};
+FLOAT_CAN_STRUCT inv_flux_feedback = {0};
+FLOAT_CAN_STRUCT inv_id_feedback = {0};
+FLOAT_CAN_STRUCT inv_iq_feedback = {0};
+FLOAT_CAN_STRUCT inv_1v5_ref = {0};
+FLOAT_CAN_STRUCT inv_2v5_ref = {0};
+FLOAT_CAN_STRUCT inv_5v_ref = {0};
+FLOAT_CAN_STRUCT inv_12v_ref = {0};
+FLOAT_CAN_STRUCT inv_post_fault_lo = {0};
+FLOAT_CAN_STRUCT inv_post_fault_hi = {0};
+FLOAT_CAN_STRUCT inv_run_fault_lo = {0};
+FLOAT_CAN_STRUCT inv_run_fault_hi = {0};
+FLOAT_CAN_STRUCT inv_torque_cmd = {0};
+FLOAT_CAN_STRUCT inv_torque_feedback = {0};
+FLOAT_CAN_STRUCT inv_power_on_timer = {0};
+FLOAT_CAN_STRUCT inv_modulation_index = {0};
+FLOAT_CAN_STRUCT inv_flux_weakening = {0};
+FLOAT_CAN_STRUCT inv_id_cmd = {0};
+FLOAT_CAN_STRUCT inv_iq_cmd = {0};
+FLOAT_CAN_STRUCT inv_torque_cmd_high = {0};
+FLOAT_CAN_STRUCT inv_torque_feedback_high = {0};
+U16_CAN_STRUCT inv_motor_speed_high = {0};
+FLOAT_CAN_STRUCT inv_dc_bus_voltage_high = {0};
+U8_CAN_STRUCT inv_vsm_state = {0};
+U8_CAN_STRUCT inv_pwm_freq = {0};
+U8_CAN_STRUCT inv_inverter_state = {0};
+U8_CAN_STRUCT inv_relay_state = {0};
+U8_CAN_STRUCT inv_states_byte4 = {0};
+U8_CAN_STRUCT inv_states_byte5 = {0};
+U8_CAN_STRUCT inv_states_byte6 = {0};
+U8_CAN_STRUCT inv_states_byte7 = {0};
+FLOAT_CAN_STRUCT dummy_param = {0};
+
+
+// this is the struct that will be used to reference based on ID
+void* all_parameter_structs[NUM_OF_PARAMETERS] =
+{
+    &can_command,
+    &bat_0_temp,
+    &bat_1_temp,
+    &bat_2_temp,
+    &bat_3_temp,
+    &bat_4_temp,
+    &bat_5_temp,
+    &bat_6_temp,
+    &bat_7_temp,
+    &bat_8_temp,
+    &bat_9_temp,
+    &bat_10_temp,
+    &bat_11_temp,
+    &bat_12_temp,
+    &bat_13_temp,
+    &bat_14_temp,
+    &bat_15_temp,
+    &bat_16_temp,
+    &bat_17_temp,
+    &bat_18_temp,
+    &bat_19_temp,
+    &bat_20_temp,
+    &bat_21_temp,
+    &bat_22_temp,
+    &bat_23_temp,
+    &bat_24_temp,
+    &bat_25_temp,
+    &bat_26_temp,
+    &bat_27_temp,
+    &bat_28_temp,
+    &bat_29_temp,
+    &bat_30_temp,
+    &bat_31_temp,
+    &bat_32_temp,
+    &bat_33_temp,
+    &bat_34_temp,
+    &bat_35_temp,
+    &bat_36_temp,
+    &bat_37_temp,
+    &bat_38_temp,
+    &bat_39_temp,
+    &bat_40_temp,
+    &bat_41_temp,
+    &bat_42_temp,
+    &bat_43_temp,
+    &bat_44_temp,
+    &bat_45_temp,
+    &bat_46_temp,
+    &bat_47_temp,
+    &bat_48_temp,
+    &bat_49_temp,
+    &bat_50_temp,
+    &bat_51_temp,
+    &bat_52_temp,
+    &bat_53_temp,
+    &bat_54_temp,
+    &bat_55_temp,
+    &bat_56_temp,
+    &bat_57_temp,
+    &bat_58_temp,
+    &bat_59_temp,
+    &bat_60_temp,
+    &bat_61_temp,
+    &bat_62_temp,
+    &bat_63_temp,
+    &bat_64_temp,
+    &bat_65_temp,
+    &bat_66_temp,
+    &bat_67_temp,
+    &bat_68_temp,
+    &bat_69_temp,
+    &bat_70_temp,
+    &bat_71_temp,
+    &bat_72_temp,
+    &bat_73_temp,
+    &bat_74_temp,
+    &bat_75_temp,
+    &bat_76_temp,
+    &bat_77_temp,
+    &bat_78_temp,
+    &bat_79_temp,
+    &bat_80_temp,
+    &bat_81_temp,
+    &bat_82_temp,
+    &bat_83_temp,
+    &bat_84_temp,
+    &bat_85_temp,
+    &bat_86_temp,
+    &bat_87_temp,
+    &bat_min_temp,
+    &bat_ave_temp,
+    &bat_max_temp,
+    &pdm_1_current,
+    &pdm_2_current,
+    &pdm_3_current,
+    &pdm_4_current,
+    &pdm_5_current,
+    &pdm_6_current,
+    &pdm_7_current,
+    &pdm_8_current,
+    &pdm_9_current,
+    &pdm_10_current,
+    &pdm_11_current,
+    &pdm_12_current,
+    &pdm_13_current,
+    &pdm_14_current,
+    &pdm_15_current,
+    &pdm_16_current,
+    &pdm_17_current,
+    &pdm_18_current,
+    &pdm_19_current,
+    &pdm_20_current,
+    &pdm_voltage,
+    &pdm_temp,
+    &vcu_apps1,
+    &vcu_apps2,
+    &vcu_current1,
+    &vcu_current2,
+    &vcu_brake_pressure_front,
+    &vcu_wsfl,
+    &vcu_wsfr,
+    &vcu_spfl,
+    &vcu_spfr,
+    &vcu_sprl,
+    &vcu_sprr,
+    &vcu_sas,
+    &vcu_bat_volts,
+    &vcu_rtd_btn,
+    &vcu_rtd_bzzr_duty,
+    &vcu_pump_duty,
+    &vcu_fan_duty,
+    &vcu_bl_duty,
+    &vcu_status,
+    &inv_temp_igbt_a,
+    &inv_temp_igbt_b,
+    &inv_temp_igbt_c,
+    &inv_temp_gate_driver,
+    &inv_temp_ctrl_board,
+    &inv_temp_rtd_1,
+    &inv_temp_rtd_2,
+    &inv_temp_rtd_3,
+    &inv_temp_rtd_4,
+    &inv_temp_rtd_5,
+    &inv_motor_temp,
+    &inv_torque_shudder,
+    &inv_digital_in_1,
+    &inv_digital_in_2,
+    &inv_digital_in_3,
+    &inv_digital_in_4,
+    &inv_digital_in_5,
+    &inv_digital_in_6,
+    &inv_digital_in_7,
+    &inv_digital_in_8,
+    &inv_motor_angle,
+    &inv_motor_speed,
+    &inv_motor_ac_freq,
+    &inv_delta_resolver_filtered,
+    &inv_current_phase_a,
+    &inv_current_phase_b,
+    &inv_current_phase_c,
+    &inv_current_dc_bus,
+    &inv_dc_bus_voltage,
+    &inv_peak_output_voltage,
+    &inv_vd_voltage,
+    &inv_vq_voltage,
+    &inv_flux_cmd,
+    &inv_flux_feedback,
+    &inv_id_feedback,
+    &inv_iq_feedback,
+    &inv_1v5_ref,
+    &inv_2v5_ref,
+    &inv_5v_ref,
+    &inv_12v_ref,
+    &inv_post_fault_lo,
+    &inv_post_fault_hi,
+    &inv_run_fault_lo,
+    &inv_run_fault_hi,
+    &inv_torque_cmd,
+    &inv_torque_feedback,
+    &inv_power_on_timer,
+    &inv_modulation_index,
+    &inv_flux_weakening,
+    &inv_id_cmd,
+    &inv_iq_cmd,
+    &inv_torque_cmd_high,
+    &inv_torque_feedback_high,
+    &inv_motor_speed_high,
+    &inv_dc_bus_voltage_high,
+    &inv_vsm_state,
+    &inv_pwm_freq,
+    &inv_inverter_state,
+    &inv_relay_state,
+    &inv_states_byte4,
+    &inv_states_byte5,
+    &inv_states_byte6,
+    &inv_states_byte7,
+    &dummy_param
+    
+};
+
+// this stores the data_type for each parameter, referenced by ID
+U8 parameter_data_types[NUM_OF_PARAMETERS] =
+{
+    COMMAND,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    UNSIGNED8,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    UNSIGNED32,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    FLOATING,
+    UNSIGNED16,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    FLOATING,
+    UNSIGNED16,
+    FLOATING,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    UNSIGNED8,
+    FLOATING
+    
+};
+
+// if there are multiple busses, this shows which bus they are on
+#ifdef MULTI_BUS
+U8 module_bus_number[NUM_OF_MODULES] =
+{
+    ALL_BUSSES,
+    GCAN0,
+    GCAN2,
+    GCAN0,
+    GCAN1
+    
+};
+#endif // MULTI_BUS
+
+// End of GopherCAN_ids.c
+
